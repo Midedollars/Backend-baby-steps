dd961fe665dd960157080ef6884fa1e3b1c7fa35
Midedollars <olanrewajuolamide100@gmail.com>
2022-01-14 18:40:52 +0800

    backend practice

---
+++ index.js
@@ -0,0 +1,34 @@
+// for (let i=0; i<=10; i++) {
+//     console.log(`${i} ${Math.pow(i,2)} ${Math.pow(i,3)}`)
+// }
+
+// for(let i = 0; i<=10; i++){
+//     console.log(`${i} ${Math.pow(i,2)} ${Math.pow(i,3)}`)
+// }
+
+// const arr = []
+// console.log(arr)
+
+// const numbers = [0, 3.14, 9.81, 37, 98.6, 100]
+// const fruits = ['banana', 'orange', 'mango', 'lemon']
+// console.log(numbers)
+// console.log(numbers.length)
+// console.log('Numbers:', numbers)
+// console.log('Number of numbers:', numbers.length)
+// console.log(fruits)
+// console.log(fruits.length)
+// console.log('Fruits:', fruits)
+// console.log('Number of fruits:', fruits.length)
+
+// const numbers = [0, 3.14, 9.81, 37, 98.6, 100]
+// let firstnumber = numbers[0]
+// console.log(numbers[0])
+
+// for (let i=0; i<=20; i++){
+//     console.log(i)
+// }
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (num of numbers){
+    console.log(num)
+}
